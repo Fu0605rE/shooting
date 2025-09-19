481e8de0027c2dbbd90a1d7ad9a0415ef97806b7
Fu0605rE <gsr-620-bgb@e-net.nara.jp>
2025-09-19 06:18:19 +0000

    javascriptの追加 ・自機の描画・移動 ・敵の追加

---
+++ game/main.js
@@ -0,0 +1,39 @@
+document.getElementById("txt").innerText="これはゲームです";
+const canvas=document.getElementById("gameCanvas");
+const ctx=canvas.getContext("2d");
+
+let x =225;
+let tama=0;
+
+window.addEventListener("keydown", (e) => {
+    if(e.key ==="ArrowLeft"){
+        x -= 10;
+    } else if (e.key === "ArrowRight"){
+        x += 10;
+    }else if(e.key=== "space"){
+        tama +=1;
+    }
+});
+
+let y1=0;
+let y2=-150;
+
+function gameLoop() {
+ctx.fillStyle= "black";
+ctx.fillRect(0,0, canvas.width, canvas.height);
+ctx.fillStyle="lightgreen";
+ctx.fillRect(150,y1,30,30);
+y1 +=3;
+ctx.fillStyle="lightgreen";
+ctx.fillRect(300,y2,30,30);
+y2 +=3;
+ctx.fillStyle= "lightblue";
+ctx.fillRect(x,480,30,30);
+if(tama>0){
+    ctx.fillStyle="white";
+    ctx.fillRect(x + 10,480 - tama*10,10,10);
+}
+requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
