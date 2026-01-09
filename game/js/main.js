aa3d7b03ef31cf8241eafd3daa58c642b48fd555
Fu0605rE <gsr-620-bgb@e-net.nara.jp>
2026-01-09 05:47:57 +0000

    1/9

--- game/js/main.js
+++ game/js/main.js
@@ -20,6 +20,23 @@ function tryShoot() {
         y: player.y,
         width: 10,
         height: 10,
+        vx: 0,
+        vy: BULLET_SPEED,
+    },
+{
+        x: player.x + player.width / 2 - 5,
+        y: player.y,
+        width: 10,
+        height: 10,
+        vx: 1,
+        vy: BULLET_SPEED,
+    },
+{
+        x: player.x + player.width / 2 - 5,
+        y: player.y,
+        width: 10,
+        height: 10,
+        vx: -1,
         vy: BULLET_SPEED,
     })
 }
@@ -40,6 +57,17 @@ window.addEventListener("keydown", (e) => {
         if (player.x < canvas.width - player.width - 10) {
             player.x += 10;
         }
+
+         } else if (e.key === "ArrowUp") {
+        if (player.y >10) {
+            player.y -= 10;
+        }
+
+         } else if (e.key === "ArrowDown") {
+        if (player.y < canvas.height - player.width - 10) {
+            player.y += 10;
+        }
+
     } else if (e.code === "Space") {
         tryShoot();
     }
@@ -49,6 +77,7 @@ function update() {
     for (let i = 0; i < bullets.length; i++) {
         const bullet = bullets[i];
         bullet.y += bullet.vy;
+        bullet.x += bullet.vx;
         if (bullet.y < 0) {
             bullets.splice(i, 1);
         }
